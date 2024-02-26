6107ce96267dadf625b6c8c5615892ae0b854ee8
Christian Tong <christian.tongcruz96@gmail.com>
2024-02-26 07:31:09 -0500

    Footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#333",
+        secondaty: "#ddd",
+      },
+    },
+  },
+  plugins: [animations],
+};
